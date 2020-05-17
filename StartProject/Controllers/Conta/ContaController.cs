f8e33fce50e3abdafcc850f39bcc3a6381428d26
Mateus Martins <mateusfm95@gmail.com>
2020-05-17 12:18:33 -0300

    Criação de usuários e instalação pacote Owin

---
+++ StartProject/Controllers/Conta/ContaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using StartProject.Models.Conta;
+using StartProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StartProject.Controllers.Conta
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbContext = new IdentityDbContext<UsuarioAplicacao>("DefaultConnection");
+                var userStore = new UserStore<UsuarioAplicacao>(dbContext);
+                var userManager = new UserManager<UsuarioAplicacao>(userStore);
+
+                var novoUsuario = new UsuarioAplicacao();
+
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+
+                userManager.Create(novoUsuario, modelo.Senha);
+
+            }
+            return View();
+        }
+    }
+}
